906fa36a7db09edbf10c0a4ae8612b42bc3f61e4
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-12-29 20:38:37 +0100

    PLATFORM_ID - Cliente y Server

--- src/app/pages/pricing-page/pricing-page.ts
+++ src/app/pages/pricing-page/pricing-page.ts
@@ -1,4 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
@@ -11,8 +12,16 @@ export default class PricingPage implements OnInit {
 
   private title = inject(Title);
   private meta = inject(Meta);
+  //private platform = inject(PLATFORM_ID);
 
   ngOnInit(): void {
+    //Si no estamos en el servidor(NO USAR ESTA VERSION ANTIGUA)
+    // if (!isPlatformServer(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+    // console.log(this.platform);
+    // console.log(isPlatformServer(this.platform));
+    // console.log(`Estamos en el ${this.platform}`);
     this.title.setTitle('Pricing Page');
     this.meta.updateTag({ name: 'decription', content: 'Este es mi Pricing Page' });
     this.meta.updateTag({ name: 'og:title', content: 'Pricing Page' });
